782ae877444458b239bfd4950f65f98d44e6ac3f
pls455 <karamhahhss123@gmail.com>
2026-09-19 22:58:24 +0300

    Add Flutter engine Maven repository to Android settings

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -27,6 +27,8 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        val flutterStorageUrl = System.getenv("FLUTTER_STORAGE_BASE_URL") ?: "https://storage.googleapis.com"
+        maven("$flutterStorageUrl/download.flutter.io")
     }
 }
 
