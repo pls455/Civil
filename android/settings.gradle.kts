93b164461a3366b6a97745a66aa628d78aa7150e
pls455 <karamhahhss123@gmail.com>
2026-09-19 22:16:24 +0300

    Phase 0/1: add android/settings.gradle.kts

---
+++ android/settings.gradle.kts
@@ -0,0 +1,14 @@
+pluginManagement {
+    repositories { google(); mavenCentral(); gradlePluginPortal() }
+}
+plugins {
+    id("dev.flutter.flutter-plugin-loader") version "1.0.0"
+    id("com.android.application") version "8.7.3" apply false
+    id("org.jetbrains.kotlin.android") version "2.1.20" apply false
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories { google(); mavenCentral() }
+}
+rootProject.name = "citizen_registry"
+include(":app")
