2b0f157cacfb545e417290968507496da410952a
pls455 <karamhahhss123@gmail.com>
2026-09-19 22:16:21 +0300

    Phase 0/1: add android/app/build.gradle.kts

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("dev.flutter.flutter-gradle-plugin")
+}
+android {
+    namespace = "com.civil.citizenregistry"
+    compileSdk = flutter.compileSdkVersion
+    ndkVersion = flutter.ndkVersion
+    defaultConfig {
+        applicationId = "com.civil.citizenregistry"
+        minSdk = 23
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+flutter { source = "../.." }
+dependencies {
+    implementation("io.github.spannm:jackcess:5.1.7")
+}
