a73f8bf7b211bfee8fea0d10178d04326cd8a885
pls455 <karamhahhss123@gmail.com>
2026-09-24 23:26:51 +0300

    Add cloud search API configuration

---
+++ lib/core/constants/cloud_api_config.dart
@@ -0,0 +1,7 @@
+class CloudApiConfig {
+  static const endpoint = 'https://gaza-c.online/api.php';
+
+  // The inspected API worked without a token. Leave empty unless the
+  // private API is configured to require one.
+  static const token = '';
+}
